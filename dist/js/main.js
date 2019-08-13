865d6866ce08fd0f292c0ba94ea2ad03f5f18f95
David Schousen <dschousen@comcast.net>
2019-08-13 11:25:25 -0500

    color update

--- dist/js/main.js
+++ dist/js/main.js
@@ -38,7 +38,7 @@ var game = (function(){
       var text = "";
       var possible = "abcdefghijklmnopqrstuvwxyz";
 
-      for (var i = 0; i < 100; i++){
+      for (var i = 0; i < 10; i++){
         text += possible.charAt(Math.floor(Math.random() * possible.length));
       }
 
